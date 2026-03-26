69c2681598923931bfbc2b344fe258cc4efa4ea2
hikamishin <hikamishin@gmail.com>
2026-03-26 21:09:51 +0800

    Add files via upload

---
+++ A1133320＿黃詡宸＿作業1/A1133320_黃詡宸_作業1.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+    <title>CS2夏令營</title>
+</head>
+<body>
+    <center><img src="cs2.jpg" width="600"></center><br>
+
+    <h1><center><font color="#99FFFF" size="6">你是否熱愛FPS？</font></center></h1>
+    <h1><center><font color="#99FFFF" size="6">你是否享受與人並肩作戰的快感？</font></center></h1>
+
+    <h3><center><font color="#99FFFF" size="4">我們有頂尖的設備</font></center></h3>
+    <h3><center><font color="#99FFFF" size="4">我們有專業的知識</font></center></h3>
+    <h3><center><font color="#99FFFF" size="4">我們有熱心的老師</font></center></h3>
+
+    <h1><center><font color="#99FFFF" size="6">不要猶豫，立刻報名吧！</font></center></h1>
+
+    <form>
+        <center>
+            姓名：<input type="text" placeholder="your name" name="nName" value=""><br><br>
+
+            性別：
+            男 <input type="radio" name="mGender" value="m">
+            女 <input type="radio" name="mGender" value="f" checked><br><br>
+
+            年齡：<input type="text" placeholder="your age" name="nAge" value=""><br><br>
+
+            遊戲經驗：<br>
+            我是新手 <input type="radio" name="mEx" value="noob"><br>
+            我玩過槍戰，沒玩過CS2 <input type="radio" name="mEx" value="soso"><br>
+            我是大佬 <input type="radio" name="mEx" value="pro"><br><br>
+
+            其他意見：<input type="text" placeholder="your op" name="nop" value=""><br><br>
+            
+            <input type="submit" value="確認送出">
+            <input type="reset" value="重新填寫">
+        </center>
+    </form>
+
+</body>
+</html>
